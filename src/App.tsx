ddffae5f7b64b1e6e56c12a332d1530efd000e92
Dennis Thandy <dennisthandy15@gmail.com>
2021-06-20 17:29:19 +0700

    add previous code 18

--- src/App.tsx
+++ src/App.tsx
@@ -21,51 +21,15 @@ function App() {
   };
 
   return (
-    <Wrapper>
-      <Container center>
+    <Container center>
+      <Wrapper>
         <Header />
         {!openAbout && <Home />}
         {openAbout && <About handleCloseAbout={handleAboutPage} />}
         <Footer handleOpenAbout={handleAboutPage} />
-      </Container>
-    </Wrapper>
+      </Wrapper>
+    </Container>
   );
 }
 
 export default App;
-
-// import React, { useState } from 'react';
-// import styled from 'styled-components';
-
-// import Container from './components/elements/Container';
-// import Footer from './components/Footer';
-// import Header from './components/Header';
-// import About from './pages/About';
-// import Home from './pages/Home';
-
-// const Wrapper = styled.main`
-//   padding-bottom: 1rem;
-//   background-color: ${(props) => props.theme.colors.yellow};
-//   min-height: 100vh;
-// `;
-
-// const App: React.FC = (): React.ReactElement => {
-//   const [openAbout, setOpenAbout] = useState<boolean>(false);
-
-//   const handleAboutPage = () => {
-//     setOpenAbout(!openAbout);
-//   };
-
-//   return (
-//     <Container center>
-//       <Wrapper>
-//         <Header />
-//         {!openAbout && <Home />}
-//         {openAbout && <About handleCloseAbout={handleAboutPage} />}
-//         <Footer handleOpenAbout={handleAboutPage} />
-//       </Wrapper>
-//     </Container>
-//   );
-// };
-
-// export default App;
